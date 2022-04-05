7768cfe0cb9d64dd7f2a6ff923f28fcbfe6da961
Mgonzal3z <81936645+Mgonzal3z@users.noreply.github.com>
2022-04-05 15:31:19 -0500

    Add files via upload

---
+++ Taller Divide y Venceras/src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,17 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.view.vista;
+
+public class Controller {
+	
+	public Controller() {
+		funcionar();
+	}
+	
+	public void funcionar() {
+		
+	}
+	
+	
+
+}
